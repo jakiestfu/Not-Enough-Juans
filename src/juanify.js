eb3652e02ac59d4d5b723b5130394bff013527af
Jacob Kelley <jakie8@gmail.com>
2015-04-30 14:24:28 -0700

    Initial commit

---
+++ src/juanify.js
@@ -0,0 +1,47 @@
+(function() {
+
+	var name = "Juan";
+
+	// Recurse through the DOM
+	walk(document.body);
+
+	function walk(node) {
+		var child, next;
+
+		switch ( node.nodeType ) {
+			case 1:  // Element
+			case 9:  // Document
+			case 11: // Document fragment
+				child = node.firstChild;
+				while (child) {
+					next = child.nextSibling;
+					walk(child);
+					child = next;
+				}
+				break;
+
+			case 3: // Text node
+				juanify(node);
+				break;
+		}
+	}
+
+	// Need moar Juans
+	function juanify(textNode) {
+		var v = textNode.nodeValue;
+
+		var words = [
+			"one", "want",
+			"wan", "whine"
+		];
+
+		for(var i=0; i<words.length; i++) {
+			v = v.replace(" " + words[i], " " + name);
+		}
+		
+		if(textNode.nodeValue !== v) {
+			textNode.nodeValue = v;
+		}
+	}
+
+})();
